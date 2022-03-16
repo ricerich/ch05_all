f2832b4c43d5bb26ea3183f98b9a746d452a6969
Joseph Yoon <ricerich@gmail.com>
2022-03-16 11:09:38 +0900

    계산기 예제까지

---
+++ ex05_03/src/main/java/com/example/ex05_03/MainActivity2.kt
@@ -0,0 +1,68 @@
+package com.example.ex05_03
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+
+        //뷰 -> 레이아웃 + 뷰(위젯)
+
+        //0.레이아웃 옵션값이 필요함(param) - 예)layout_width, layout_height
+        //1.레이아웃을 kotlin객체로만 만든다(only) : 예) LinearLayout
+        //2.레이아웃 속성 설정 , 예)backgraouncolor, orientation
+        //3.(레이아웃과 옵션값)뷰를 꽂는다 액티비티에..
+
+
+        //0.레이아웃 옵션값이 필요함(param) - 예)layout_width, layout_height
+        var option = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                                               LinearLayout.LayoutParams.MATCH_PARENT)
+
+        //1.레이아웃을 kotlin객체로만 만든다(only) : 예) LinearLayout
+        var layout1 = LinearLayout(this)//생성자 매개변수에, 액티비티(위치)가 필요하다
+
+        //2.레이아웃 속성 설정 , 예)backgraouncolor, orientation
+        layout1.orientation = LinearLayout.VERTICAL
+        layout1.setBackgroundColor(Color.rgb(0,255,0))
+
+        //3.(레이아웃과 옵션값)뷰를 꽂는다 액티비티에..
+        setContentView(layout1, option)
+
+        //위젯추가
+        //1.kotlin객체로 뷰(버튼)를 만든다.activity위치 적어줌 예)버튼 , xml객체 아님!
+        //2.뷰(버튼)의 속성을 설정한다. 예) text, backgroundcolo, textSize
+        //3.버튼을 레이아웃에 꽂는다.
+        //4.중요: 버튼 이벤트 처리. 예) setOnClickListener
+
+        //editTex
+        var edt1 = EditText(this)
+        edt1.hint = "여기에 입력하세요"
+        layout1.addView(edt1)
+
+        //1.kotlin객체로 뷰(버튼)를 만든다.생성자매개변수에 activity위치 적어줌 예)버튼 , xml객체 아님!
+        var btn1 = Button(this)
+        //2.뷰(버튼)의 속성을 설정한다. 예) text, backgroundcolo, textSize
+        btn1.text = "버튼1111"
+        btn1.setBackgroundColor(Color.CYAN)
+        //3.버튼을 레이아웃에 꽂는다.
+        layout1.addView(btn1)
+
+        //TextView
+        var tv1 = TextView(this)
+        tv1.text = "결과출력"
+        tv1.setBackgroundColor(Color.YELLOW)
+        tv1.textSize = 20f
+        layout1.addView(tv1)
+
+
+        //4.중요: 버튼 이벤트 처리. 예) setOnClickListener
+        btn1.setOnClickListener {
+//            Toast.makeText(applicationContext,"버튼클릭했네~!",Toast.LENGTH_SHORT).show()
+            tv1.text = edt1.text
+        }
+    }
+}
--- ex05_05_calc_num_button/src/main/java/com/example/ex05_04_calc_num_button/MainActivity.kt
+++ ex05_05_calc_num_button/src/main/java/com/example/ex05_04_calc_num_button/MainActivity.kt
@@ -1,11 +1,357 @@
 package com.example.ex05_04_calc_num_button
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    //1.멤버변수
+
+
+    //2.(멤버)메소드 - onCreate()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "초간단 계산기"
+
+        //1.kotlin객체 만들기
+        //2.xml객체를 가져와서, kotlin 객체에 연결(바인딩)
+        //3.kotlin객체로 이벤트처리~!
+
+        //지역변수 자리
+
+        // 1. + 2. -> kotlin 객체를 만들면서, 동시에 xml객체를 연결(바인딩)
+        var edt1: EditText = findViewById(R.id.Edit1)
+        var edt2: EditText = findViewById(R.id.Edit2)
+
+        var btn1: Button = findViewById(R.id.BtnAdd)
+        var btn2: Button = findViewById(R.id.BtnSub)
+        var btn3: Button = findViewById(R.id.BtnMul)
+        var btn4: Button = findViewById(R.id.BtnDiv)
+
+        var btn5: Button = findViewById(R.id.BtnMod)
+
+        var tv1: TextView = findViewById(R.id.TextResult)
+
+        //추가:1. + 2. 버튼 10개 추가
+//        var btnNum0: Button = findViewById(R.id.BtnNum0)
+//        var btnNum1: Button = findViewById(R.id.BtnNum1)
+//        var btnNum2: Button = findViewById(R.id.BtnNum2)
+//        var btnNum3: Button = findViewById(R.id.BtnNum3)
+//        var btnNum4: Button = findViewById(R.id.BtnNum4)
+//        var btnNum5: Button = findViewById(R.id.BtnNum5)
+//        var btnNum6: Button = findViewById(R.id.BtnNum6)
+//        var btnNum7: Button = findViewById(R.id.BtnNum7)
+//        var btnNum8: Button = findViewById(R.id.BtnNum8)
+//        var btnNum9: Button = findViewById(R.id.BtnNum9)
+
+
+        var btnIDArr = arrayOf(R.id.BtnNum0, R.id.BtnNum1, R.id.BtnNum2, R.id.BtnNum3, R.id.BtnNum4,
+                               R.id.BtnNum5, R.id.BtnNum6, R.id.BtnNum7, R.id.BtnNum8, R.id.BtnNum9)
+
+        //버튼(숫자버튼)을 "객체배열"
+        var btnNumArr = ArrayList<Button>(10)
+
+        for(i in 0..9)
+        {
+//            btnNumArr[0] = findViewById(R.id.BtnNum0)
+//            btnNumArr[1] = findViewById(R.id.BtnNum1)
+//            btnNumArr[i] = findViewById(btnIDArr[i])//이거는 일반 배열형식
+            btnNumArr.add(findViewById(btnIDArr[i]))
+        }
+
+
+
+        for(i in 0..9)
+        {
+            //추가: 3. 버튼(숫자버튼) 이벤트처리 10개~
+            btnNumArr[i].setOnClickListener {
+                //포커스가 edt1 에 있는지, edt2에 있는지 체크~!
+                if(edt1.isFocused)//edt1에 포커스가 있으면
+                {
+                    var str = edt1.text.toString() + i
+                    edt1.setText(str)
+                }
+                else if(edt2.isFocused)
+                {
+                    var str = edt2.text.toString() + i
+                    edt2.setText(str)
+                }
+                else//포커스 가 딴데 있으면
+                {
+                    Toast.makeText(applicationContext,"포커스 선택 해주세요", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+//        btnNum1.setOnClickListener {
+//            //포커스가 edt1 에 있는지, edt2에 있는지 체크~!
+//            if(edt1.isFocused)//edt1에 포커스가 있으면
+//            {
+//                var str = edt1.text.toString() + "1"
+//                edt1.setText(str)
+//            }
+//            else if(edt2.isFocused)
+//            {
+//                var str = edt2.text.toString() + "1"
+//                edt2.setText(str)
+//            }
+//            else//포커스 가 딴데 있으면
+//            {
+//                Toast.makeText(applicationContext,"포커스 선택 해주세요", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        btnNum2.setOnClickListener {
+//            //포커스가 edt1 에 있는지, edt2에 있는지 체크~!
+//            if(edt1.isFocused)//edt1에 포커스가 있으면
+//            {
+//                var str = edt1.text.toString() + "2"
+//                edt1.setText(str)
+//            }
+//            else if(edt2.isFocused)
+//            {
+//                var str = edt2.text.toString() + "2"
+//                edt2.setText(str)
+//            }
+//            else//포커스 가 딴데 있으면
+//            {
+//                Toast.makeText(applicationContext,"포커스 선택 해주세요", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        btnNum3.setOnClickListener {
+//            //포커스가 edt1 에 있는지, edt2에 있는지 체크~!
+//            if(edt1.isFocused)//edt1에 포커스가 있으면
+//            {
+//                var str = edt1.text.toString() + "3"
+//                edt1.setText(str)
+//            }
+//            else if(edt2.isFocused)
+//            {
+//                var str = edt2.text.toString() + "3"
+//                edt2.setText(str)
+//            }
+//            else//포커스 가 딴데 있으면
+//            {
+//                Toast.makeText(applicationContext,"포커스 선택 해주세요", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        btnNum4.setOnClickListener {
+//            //포커스가 edt1 에 있는지, edt2에 있는지 체크~!
+//            if(edt1.isFocused)//edt1에 포커스가 있으면
+//            {
+//                var str = edt1.text.toString() + "4"
+//                edt1.setText(str)
+//            }
+//            else if(edt2.isFocused)
+//            {
+//                var str = edt2.text.toString() + "4"
+//                edt2.setText(str)
+//            }
+//            else//포커스 가 딴데 있으면
+//            {
+//                Toast.makeText(applicationContext,"포커스 선택 해주세요", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        btnNum5.setOnClickListener {
+//            //포커스가 edt1 에 있는지, edt2에 있는지 체크~!
+//            if(edt1.isFocused)//edt1에 포커스가 있으면
+//            {
+//                var str = edt1.text.toString() + "5"
+//                edt1.setText(str)
+//            }
+//            else if(edt2.isFocused)
+//            {
+//                var str = edt2.text.toString() + "5"
+//                edt2.setText(str)
+//            }
+//            else//포커스 가 딴데 있으면
+//            {
+//                Toast.makeText(applicationContext,"포커스 선택 해주세요", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        btnNum6.setOnClickListener {
+//            //포커스가 edt1 에 있는지, edt2에 있는지 체크~!
+//            if(edt1.isFocused)//edt1에 포커스가 있으면
+//            {
+//                var str = edt1.text.toString() + "6"
+//                edt1.setText(str)
+//            }
+//            else if(edt2.isFocused)
+//            {
+//                var str = edt2.text.toString() + "6"
+//                edt2.setText(str)
+//            }
+//            else//포커스 가 딴데 있으면
+//            {
+//                Toast.makeText(applicationContext,"포커스 선택 해주세요", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        btnNum7.setOnClickListener {
+//            //포커스가 edt1 에 있는지, edt2에 있는지 체크~!
+//            if(edt1.isFocused)//edt1에 포커스가 있으면
+//            {
+//                var str = edt1.text.toString() + "7"
+//                edt1.setText(str)
+//            }
+//            else if(edt2.isFocused)
+//            {
+//                var str = edt2.text.toString() + "7"
+//                edt2.setText(str)
+//            }
+//            else//포커스 가 딴데 있으면
+//            {
+//                Toast.makeText(applicationContext,"포커스 선택 해주세요", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        btnNum8.setOnClickListener {
+//            //포커스가 edt1 에 있는지, edt2에 있는지 체크~!
+//            if(edt1.isFocused)//edt1에 포커스가 있으면
+//            {
+//                var str = edt1.text.toString() + "8"
+//                edt1.setText(str)
+//            }
+//            else if(edt2.isFocused)
+//            {
+//                var str = edt2.text.toString() + "8"
+//                edt2.setText(str)
+//            }
+//            else//포커스 가 딴데 있으면
+//            {
+//                Toast.makeText(applicationContext,"포커스 선택 해주세요", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        btnNum9.setOnClickListener {
+//            //포커스가 edt1 에 있는지, edt2에 있는지 체크~!
+//            if(edt1.isFocused)//edt1에 포커스가 있으면
+//            {
+//                var str = edt1.text.toString() + "9"
+//                edt1.setText(str)
+//            }
+//            else if(edt2.isFocused)
+//            {
+//                var str = edt2.text.toString() + "9"
+//                edt2.setText(str)
+//            }
+//            else//포커스 가 딴데 있으면
+//            {
+//                Toast.makeText(applicationContext,"포커스 선택 해주세요", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+
+        //3.kotlin객체에 이벤트 처리
+
+        btn1.setOnTouchListener { view, motionEvent ->
+
+            var val1 = edt1.text;
+            var val2 = edt2.text;
+
+            var str1 = val1.toString()
+            var str2 = val2.toString()
+
+//            int n1 = Integer.parseInt(str1)
+
+//            if(str1.equals("") || str2.equals(""))
+            if (str1.trim() == "" || str2.trim() == "") {
+                Toast.makeText(applicationContext, "값이 비었습니다!", Toast.LENGTH_SHORT).show()
+            } else {
+//                var n1 = str1.toInt()
+//                var n2 = str2.toInt()
+//
+//                var n3 = n1 + n2;
+                var n1 = str1.toDouble()
+                var n2 = str2.toDouble()
+
+                var n3 = n1 + n2;
+
+                tv1.setText("계산 결과 : " + n3)
+            }
+
+
+
+            false
+        }
+
+        btn2.setOnTouchListener { view, motionEvent ->
+            var n1 = edt1.text.toString().toDouble()
+            var n2 = edt2.text.toString().toDouble()
+            var n3 = n1 - n2
+
+            tv1.setText("계산 결과 : " + n3)
+
+            false
+        }
+
+        btn3.setOnTouchListener { view, motionEvent ->
+            var n1 = edt1.text.toString().toDouble()
+            var n2 = edt2.text.toString().toDouble()
+            var n3 = n1 * n2
+
+            tv1.setText("계산 결과 : " + n3)
+
+            false
+        }
+
+//        btn4.setOnTouchListener { view, motionEvent ->
+//            var n1 = edt1.text.toString().toInt()
+//            var n2 = edt2.text.toString().toInt()
+//            var n3 = n1 / n2
+//
+//            tv1.setText("계산 결과 : "+n3)
+//
+//            false
+//        }
+
+        btn4.setOnClickListener {
+            var val1 = edt1.text;
+            var val2 = edt2.text;
+
+            var str1 = val1.toString()
+            var str2 = val2.toString()
+
+//            int n1 = Integer.parseInt(str1)
+
+//            if(str1.equals("") || str2.equals(""))
+            if (str1.trim() == "" || str2.trim() == "") {
+                Toast.makeText(applicationContext, "값이 비었습니다!", Toast.LENGTH_SHORT).show()
+            } else//str1이 공백이 아니면, 계산하라~!
+            {
+                if (str2.trim() == "0") {
+                    Toast.makeText(applicationContext, "0으로 못나눔~!", Toast.LENGTH_SHORT).show()
+                } else {
+                    //                var n1 = str1.toInt()
+//                var n2 = str2.toInt()
+//
+//                var n3 = n1 + n2;
+                    var n1 = str1.toDouble()
+                    var n2 = str2.toDouble()
+
+                    var n3 = n1 / n2;
+                    n3 = (n3 * 100).toInt() / 100.0
+
+                    tv1.setText("계산 결과 : " + n3)
+                }
+
+            }
+        }
+
+        btn5.setOnClickListener {
+            var n1 = edt1.text.toString().toDouble()
+            var n2 = edt2.text.toString().toDouble()
+            var n3 = n1 % n2
+
+            tv1.setText("계산 결과 : " + n3)
+        }
     }
 }
